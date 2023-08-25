a7f52ada2d453534be050c3f3a6c381ae3a26154
Hasan DAĞ <hdag@havelsan.com.tr>
2023-08-25 16:39:29 +0300

    first commit

---
+++ my_blossom-application-front-end/src/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const connectToDatabase = async () => {
+    try {
+        console.log("tring to connect")
+        const connetion = await mongoose.connect(
+            "mongodb+srv://hasandg:ScgMvvMcZYhfHASV@myapp.vnlq2wd.mongodb.net/?retryWrites=true&w=majority"
+        )
+        if(connetion){
+            console.log("Connection established")
+        }else{
+            console.log("not connected!!!!")
+        }
+    } catch (error) {
+        console.log('eroor in connectToDatabase', error)
+        throw error
+    }
+}
+
+export default connectToDatabase
