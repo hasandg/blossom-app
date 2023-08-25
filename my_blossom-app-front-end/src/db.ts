d8b0323aeb54ae7732aa8d4f4f7fae347dd0b0b6
hasandg <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>
2023-08-25 22:10:24 +0300

    my first commit

--- my_blossom-app-front-end/src/db.ts
+++ my_blossom-app-front-end/src/db.ts
@@ -4,7 +4,7 @@ const connectToDatabase = async () => {
     try {
         console.log("tring to connect")
         const connetion = await mongoose.connect(
-            "mongodb+srv://hasandg:ScgMvvMcZYhfHASV@myapp.vnlq2wd.mongodb.net/?retryWrites=true&w=majority"
+            "mongodb+srv://hasandg:QycdNszCy3WoargE@myapp.vnlq2wd.mongodb.net/?retryWrites=true&w=majority"
         )
         if(connetion){
             console.log("Connection established")
