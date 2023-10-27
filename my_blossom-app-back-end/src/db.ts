a56be3062e881c36170d485842435d771098c40e
hasandg <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>
2023-10-27 16:59:10 +0300

    added task function

--- my_blossom-app-back-end/src/db.ts
+++ my_blossom-app-back-end/src/db.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose"
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      //process.env.MONGODB_URI
-      "mongodb+srv://hasandg:LwYOWvAswOKh2Ce8@myapp.vnlq2wd.mongodb.net/?retryWrites=true&w=majority"
+      process.env.MONGODB_URI
+      //"mongodb+srv://hasandg:LwYOWvAswOKh2Ce8@myapp.vnlq2wd.mongodb.net/?retryWrites=true&w=majority"
     )
 
     if (connection) {
